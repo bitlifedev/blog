f2cfb0502118b0c9773b245f494613045476cc25
justin <justin@bitlife.dev>
2022-03-25 23:13:42 -0400

    Initial commit

---
+++ src/test/kotlin/dev/bitlife/blog/BlogApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.bitlife.blog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
